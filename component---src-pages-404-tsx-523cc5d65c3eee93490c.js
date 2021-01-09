b3771626967ae1fb95dc558d2c24c5abfff64470
Loc Phan <phanhoangloc@gmail.com>
2021-01-09 18:06:00 +0700

    Updates

---
+++ component---src-pages-404-tsx-523cc5d65c3eee93490c.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[17],{"i6+/":function(e,t,o){"use strict";o.r(t);var n=o("2A+t"),a=o("izhR"),c=o("bz+c"),i=o("5hlw");t.default=function(){return Object(n.c)(c.a,null,Object(n.c)(i.a,{title:"Page Not Found",description:"404 Page Not Found"}),Object(n.c)(a.d,{as:"h1",variant:"styles.h1"},"Page Not Found"),Object(n.c)("section",{sx:{my:5,variant:"layout.content"}},"Oops! The page you are looking for has been removed or relocated."))}}}]);
+//# sourceMappingURL=component---src-pages-404-tsx-523cc5d65c3eee93490c.js.map
--- sw.js
+++ sw.js
@@ -27,23 +27,23 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-3091259acf2f6625f1f2.js"
+    "url": "webpack-runtime-9ba625d912df7aeb5941.js"
   },
   {
-    "url": "framework-beda7bde9d7dca0755ce.js"
+    "url": "framework-2d6b7d2e2be378715b78.js"
   },
   {
-    "url": "app-a7df370723c4d50824e0.js"
+    "url": "app-3c52c2f73d88de43df47.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "e96c76b8b38fdecc200fa0c5179aebb1"
+    "revision": "aba83de12fe3405568ee32b7dfd982ff"
   },
   {
-    "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-b0556ce5127c1a3e2490.js"
+    "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-780d0321bb4481125aff.js"
   },
   {
-    "url": "polyfill-d315eb99cbdcd89ab179.js"
+    "url": "polyfill-5e822adfa84090d77c08.js"
   },
   {
     "url": "manifest.json",
@@ -143,7 +143,7 @@ const navigationRoute = new NavigationRoute(async ({ event }) => {
   // Check for resources + the app bundle
   // The latter may not exist if the SW is updating to a new version
   const resources = await idbKeyval.get(`resources:${pathname}`)
-  if (!resources || !(await caches.match(`/app-a7df370723c4d50824e0.js`))) {
+  if (!resources || !(await caches.match(`/app-3c52c2f73d88de43df47.js`))) {
     return await fetch(event.request)
   }
 
