a912155a5985a635d760d07c6b4d584d726e5b0c
Loc Phan <phanhoangloc@gmail.com>
2021-01-11 01:12:50 +0700

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-140ef3ac0be31f964f31.js"
+    "url": "webpack-runtime-6280933ee132d96cb98d.js"
   },
   {
     "url": "framework-02088522f04f6be47b0f.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "314ea5702418d841b1a3267b28ed39fc"
+    "revision": "6792bf6110f19385cb59f114bf29a9f0"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-dec0309332051048554e.js"
