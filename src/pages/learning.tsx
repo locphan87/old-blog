0dd2da5ea6717bf4c6b9842ee8545070982a04bb
Loc Phan <phanhoangloc@gmail.com>
2021-02-17 17:40:32 +0700

    feat: update learning

--- src/pages/learning.tsx
+++ src/pages/learning.tsx
@@ -2,9 +2,11 @@
 import { jsx, Heading } from 'theme-ui'
 import { graphql } from 'gatsby'
 import * as R from 'ramda'
+import { CheckCircle, Update, HourglassEmpty, Person } from '@material-ui/icons'
 
 import Layout from '../@lekoarts/gatsby-theme-minimal-blog/components/layout'
 import SEO from '../@lekoarts/gatsby-theme-minimal-blog/components/seo'
+import { formatDate } from '../utils'
 
 export const query = graphql`
   query {
@@ -32,22 +34,31 @@ export const query = graphql`
 
 const renderItem = (item) => {
   const length = 80
+  const desc: string = R.propOr('', 'description', item)
   const trimmedDesc =
-    item.description.length > length
-      ? item.description.substring(0, length - 3) + '...'
-      : item.description
+    desc.length > length ? desc.substring(0, length - 3) + '...' : desc
   return (
     <article
       key={item.id}
       className="p-4 shadow-lg border border-dotted border-gray-600"
     >
       <header className="flex justify-between text-sm">
-        <div className="flex">
-          <div className="text-white bg-blue-600 px-2">{item.sourceId}</div>
-          <div className="ml-2 text-white bg-gray-600 px-2">{item.type}</div>
-        </div>
-        <div>{item.updated}</div>
+        <div className="text-white bg-blue-600 px-2">{item.sourceId}</div>
+        <div className="text-white bg-gray-600 px-2">{item.type}</div>
       </header>
+      <div className="flex py-2 justify-between items-center text-sm text-green-600">
+        {item.updated && (
+          <div className="flex items-center">
+            <Update />
+            <div className="ml-2">{formatDate(item.updated)}</div>
+          </div>
+        )}
+        {item.duration && (
+          <div className="flex items-center">
+            <HourglassEmpty /> {item.duration}
+          </div>
+        )}
+      </div>
       {item.image && (
         <div className="pt-2 text-center">
           <img className="max-w-full max-h-24" src={item.image} />
@@ -57,19 +68,25 @@ const renderItem = (item) => {
         <a href={item.link} target="_blank">
           {item.title}
         </a>
-        <div className="text-sm" title={item.description}>
-          {trimmedDesc}
+        {item.description && (
+          <div className="text-sm" title={item.description}>
+            {trimmedDesc}
+          </div>
+        )}
+      </div>
+      <div className="flex pb-2 justify-between items-center text-sm text-green-600">
+        <div className="flex items-center">
+          <Person />
+          <div className="ml-2">{item.authors}</div>
         </div>
       </div>
-      <div className="flex pb-2 justify-between text-sm text-green-600">
-        <div className="">{item.authors}</div>
-        <div>{item.duration}</div>
-      </div>
-      <div className="flex justify-between text-sm text-red-600">
-        <div className="">
-          {item.status.toUpperCase()}
-          {item.completed && <span> ({item.completed})</span>}
-        </div>
+      <div className="flex justify-between items-center text-sm">
+        {item.completed && (
+          <div className="flex items-center text-yellow-600">
+            <CheckCircle />
+            <div className="ml-2">{formatDate(item.completed)}</div>
+          </div>
+        )}
         {item.certificate && (
           <div>
             <a href={item.certificate} target="_blank">
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,5 +1,11 @@
+import dayjs from 'dayjs'
+
+const formatString = 'DD/MM/YYYY'
+const formatDate = (s: string): string => {
+  return dayjs(s).format(formatString)
+}
 const getEnumKey = (value: any, enumType: any): string => {
   return Object.keys(enumType)[Object.values(enumType).indexOf(value)]
 }
 
-export { getEnumKey }
+export { getEnumKey, formatDate }
